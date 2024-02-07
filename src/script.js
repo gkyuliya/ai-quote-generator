b7e1ab614862f72640cdfd5c4207c8f31a91b5cf
Yuliya <127958986+gkyuliya@users.noreply.github.com>
2024-02-07 15:13:08 +0100

    added basic Javascript and typewriter plugin to the project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: ["Everything blooms in its own time."],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+quoteElement = document.querySelector(".quote-generator-form");
+quoteElement.addEventListener("submit", generateQuote);
