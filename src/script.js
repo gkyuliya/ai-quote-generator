ec5c4eeec251af3d38cbf303446515c98d7a4da5
Yuliya <127958986+gkyuliya@users.noreply.github.com>
2024-02-22 15:36:24 +0100

    Improved UX and css changes

--- src/script.js
+++ src/script.js
@@ -12,13 +12,15 @@ function generateQuote(event) {
 
   userInput = document.querySelector("#user-input");
   apiKey = "5bd39438od4a5b74b16a2a5f3d6905t4";
-  prompt = `Generate an inspirational quote about ${userInput}`;
+  prompt = `Generate a unique and short mental health quote about ${userInput.value}`;
   context =
-    "User instructions are: you're a philosopher and an inspirational quote expert about mental health, femininity, inspiration, motivation, self-love and confidence. Your mission is to generate a 4 line quote and separate each line with a <br  />. Make sure to follow the user instructions. At the end of the quote sign with 'Sakura AI' inside <strong> element";
+    "User instructions are: you're a psychologist and an inspirational quote expert about mental health. Your mission is to generate a 4 line quote and separate each line with a <br  />. Make sure to follow the user instructions. At the end of the quote sign with 'Sakura AI' inside <strong> element";
   apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-  console.log("Generating quote");
-  console.log(`prompt: ${prompt}`);
-  console.log(`context: ${context}`);
+
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating">Generating a quote about ${userInput.value}!</div>`;
+
   axios.get(apiURL).then(displayQuote);
 }
 
